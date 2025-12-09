1b6bdc6c4604396c877746978da930197a561aa7
IsmaelJC <ismael@bemmbo.com>
2025-12-09 10:13:33 -0300

    feat: wrote motivating example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello TypeScript");
+import { getAllObjectsAndSumPrices } from "./motivating-example.ts";
+
+const main = async () => {
+  const ids = Array.from({ length: 20 }, (_, i) => i.toString());
+
+  const result = await getAllObjectsAndSumPrices(ids);
+
+  console.log(result);
+};
+
+main();
---
+++ src/motivating-example.ts
@@ -0,0 +1,67 @@
+import axios from "axios";
+
+const CHUNK_SIZE = 6;
+
+const formatQueryString = (ids: string[]) => {
+  let queryString = "";
+
+  for (let i = 0; i < ids.length; i++) {
+    const id = ids[i];
+    queryString += `id=${id}`;
+
+    if (i < ids.length - 1) {
+      queryString += "&";
+    }
+  }
+
+  return queryString;
+};
+
+const chunksOf = (array: any[], size: number) => {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += size) {
+    const chunk = [];
+
+    for (let j = 0; j < size; j++) {
+      chunk.push(array[i + j]);
+    }
+    chunks.push(chunk);
+  }
+  return chunks;
+};
+
+const getObjects = async (ids: string[]) => {
+  const response = await axios.get(
+    `https://api.restful-api.dev/objects?${formatQueryString(ids)}`
+  );
+
+  return response.data;
+};
+
+const getAllObjects = async (ids: string[]) => {
+  const chunks = chunksOf(ids, CHUNK_SIZE);
+
+  let promises = [];
+
+  for (const chunk of chunks) {
+    promises.push(getObjects(chunk));
+  }
+
+  return Promise.all(promises);
+};
+
+const sumAllPrices = (objects: any[]) => {
+  let prices = 0;
+
+  for (const object of objects) {
+    prices += object.data.price;
+  }
+
+  return prices;
+};
+
+export const getAllObjectsAndSumPrices = async (ids: string[]) => {
+  const objects = await getAllObjects(ids);
+
+  return sumAllPrices(objects);
+};
